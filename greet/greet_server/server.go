b39b80fefe9c44de9dfd582c21c9218a44eebfb9
parsa <akbariparsa1209@gmail.com>
2020-09-01 12:04:51 +0430

    Added server code for Server Streaming RPC.

--- greet/greet_server/server.go
+++ greet/greet_server/server.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"strconv"
+	"time"
 
 	"github.com/parsaakbari1209/gRPC-Golang-Hello-World/greet/greetpb"
 	"google.golang.org/grpc"
@@ -12,14 +14,30 @@ import (
 
 type server struct{}
 
-func (*server) Greet(ctx context.Context, req *greetpb.GreetingRequest) (*greetpb.GreetingResponse, error) {
+func (*server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.GreetResponse, error) {
+	fmt.Println("Greet (Unary RPC) invoked.")
 	firstName := req.GetGreeting().GetFirstName()
 	// lastName := req.GetGreeting().GetLastName()
 	result := "Hello " + firstName
-	res := &greetpb.GreetingResponse{
+	res := &greetpb.GreetResponse{
 		Result: result,
 	}
 	return res, nil
+}
+
+func (*server) GreetManyTimes(req *greetpb.GreetManyTimesRequest, stream greetpb.GreetService_GreetManyTimesServer) error {
+	fmt.Println("Greet (Server Streaming RPC) invoked.")
+	firstName := req.GetGreeting().GetFirstName()
+	// lastName := req.GetGreeting.GetLastName()
+	for i := 0; i < 10; i++ {
+		result := "Hello " + firstName + " number " + strconv.Itoa(i)
+		res := &greetpb.GreetManyTimesResponse{
+			Result: result,
+		}
+		stream.Send(res)
+		time.Sleep(1000 * time.Millisecond)
+	}
+	return nil
 }
 
 func main() {
